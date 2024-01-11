477ed0c8fc44131e562b4c41237320da478a4bb0
jacksonz16 <144455915+jacksonz16@users.noreply.github.com>
2024-01-10 19:58:50 -0500

    121

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,16 +1,53 @@
 import '../css/style.css'
 import { DOMSelector } from '../js/dom.js'
-const url = "https://rickandmortyapi.com/api/character/?name=";
+const URL = "https://rickandmortyapi.com/api/character/?name=";
 
-async function getData(url) {
+async function getData(URL) {
   try {
-    const response = await fetch(url);
-    const Data = await response.json();
-    return Data.results;
+    const response = await fetch(URL);
+    if (!response.ok) {
+      DOMSelector.cards.innerHTML = "";
+      DOMSelector.error.innerHTML = "";
+      if (response.status === 400) {
+        throw new Error("Error 400: Bad Request" + `<p>WHAT ARE YOU ASKING FOR.</p>`
+        );
+      } else if (response.status === 403) {
+        throw new Error("Error 403: Access Forbidden" + `<p>Your device SEEMS TO HAVE BEEN BANED BY THE API</p>`
+        );
+      } else if (response.status === 404) {
+        throw new Error("Error 404: Not Found" + `<p>SPELL THE NAMES CORRECTLY<p>`
+        );
+      } else {
+        throw new Error(`Error ${response.status}`);
+      };
+    };
+    const data = await response.json();
+  DOMSelectors.cards.innerHTML = "";
+  DOMSelectors.error.innerHTML = "";
+  data.forEach(characters => {
+    DOMSelectors.cards.insertAdjacentHTML(
+      "beforeend",
+      `
+        <div class="card">
+        <h2 class="characterName">${characters.name}</h2>
+        <img src="${characters.image}" alt="${characters.name}"class="img"/>
+        <div class="text">
+        <h3>${characters.status}</h3>
+        <h3>${characters.origin.name}</h3>
+        </div>
+        `
+      );
+    });
   } catch (error) {
     console.log(error);
-  }
-}
+    DOMSelectors.error.insertAdjacentHTML(
+      "beforeend",
+      `
+        <h3>${error.message}</h3>
+      `
+    );
+  };
+};
 //reminder add some error codes if there is a wrong input//
 DOMSelector.searchForm.addEventListener("submit", function(event) {
   event.preventDefault();
@@ -19,26 +56,27 @@ DOMSelector.searchForm.addEventListener("submit", function(event) {
   getData(URL);
 });
 
-function defaultCardGenerate(filtercharacters) {
-    filtercharacters.forEach((characters) => {
-      DOMSelector.cards.insertAdjacentHTML(
-        "beforeend",
-        `<div class="card" id="${characters.name
-          .toLowerCase()
-          .replace(/ /g, "")}">
-          <h2 class="characterName">${characters.name}</h2>
-          <img src="${characters.image}" alt="${characters.name}"class="img"/>
-          <div class="text">
-          <h3>${characters.status}</h3>
-          <h3>${characters.origin.name}</h3>
-          </div>
-      </div>`
-      );
-    });
-  }
+// function defaultCardGenerate(filtercharacters) {
+//     filtercharacters.forEach((characters) => {
+//       DOMSelector.cards.insertAdjacentHTML(
+//         "beforeend",
+//         `<div class="card" id="${characters.name
+//           .toLowerCase()
+//           .replace(/ /g, "")}">
+//           <h2 class="characterName">${characters.name}</h2>
+//           <img src="${characters.image}" alt="${characters.name}"class="img"/>
+//           <div class="text">
+//           <h3>${characters.status}</h3>
+//           <h3>${characters.origin.name}</h3>
+//           </div>
+//       </div>`
+//       );
+//     });
+//   }
 
   async function main() {
-    var filtercharacters = await getData(url);
+    var filtercharacters = await getData(URL);
     defaultCardGenerate(filtercharacters);
   }
   main();
+
