bab0eb877efee55e18df2d6e909ad9725ccfbe84
jacksonz16 <144455915+jacksonz16@users.noreply.github.com>
2024-01-10 19:56:05 -0500

    1231231231231

--- vite-project/js/dom.js
+++ vite-project/js/dom.js
@@ -1,10 +1,9 @@
-export const DOMselectors = {
-    body: document.body,
-    toggleButton: document.getElementById("toggle-button"),
-    allFilter: document.getElementById("filter-all"),
-    ShinijiFilter: document.getElementById("filter-Shiniji"),
-    REIFilter: document.getElementById("filter-REI"),
-    AsukaFilter: document.getElementById("filter-Asuka"),
-  
-    menu: document.getElementById("menu"),
-}
+export const DOMSelector = {
+    cards: document.querySelector(".cards"),
+    search: document.getElementById("search"),
+     input: document.getElementById("input"),
+     card: document.getElementById("card"),
+     searchForm: document.getElementById("search"),
+     searchBar: document.getElementById("search-bar"),
+     error: document.getElementById("error"),
+  };
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,53 +1,44 @@
 import '../css/style.css'
-import { DOMselectors } from './dom.js'
-import { Merch } from './menu.js'
+import { DOMSelector } from '../js/dom.js'
+const url = "https://rickandmortyapi.com/api/character/?name=";
 
-function toggleColors() {
-    if (DOMselectors.body.classList.contains("light")) {
-        DOMselectors.body.classList.remove("light");
-        DOMselectors.body.classList.add("dark");
-    } else if (DOMselectors.body.classList.contains("dark")) {
-        DOMselectors.body.classList.remove("dark");
-        DOMselectors.body.classList.add("light");
-    };
-};
-DOMselectors.toggleButton.addEventListener("click", toggleColors);
+async function getData(url) {
+  try {
+    const response = await fetch(url);
+    const Data = await response.json();
+    return Data.results;
+  } catch (error) {
+    console.log(error);
+  }
+}
+//reminder add some error codes if there is a wrong input//
+DOMSelector.searchForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+  const  character = DOMSelector.searchBar.value
+  const URL = "https://rickandmortyapi.com/api/character/?name="+character
+  getData(URL);
+});
 
-function filterFor(array) {
-    DOMselectors.menu.innerHTML = "";
-    array.forEach(eva => {
-        DOMselectors.menu.insertAdjacentHTML(
-            "beforeend",
-            `
-                <div class="menu-item">
-                    <img src="${eva.image}" alt="" class="item-img">
-                    <h2 class="item-name">${eva.name}</h2>
-                </div>
-            `
-        );
+function defaultCardGenerate(filtercharacters) {
+    filtercharacters.forEach((characters) => {
+      DOMSelector.cards.insertAdjacentHTML(
+        "beforeend",
+        `<div class="card" id="${characters.name
+          .toLowerCase()
+          .replace(/ /g, "")}">
+          <h2 class="characterName">${characters.name}</h2>
+          <img src="${characters.image}" alt="${characters.name}"class="img"/>
+          <div class="text">
+          <h3>${characters.status}</h3>
+          <h3>${characters.origin.name}</h3>
+          </div>
+      </div>`
+      );
     });
-};
-filterFor(Merch);
+  }
 
-function filterAll() {
-    filterFor(Merch);
-};
-DOMselectors.allFilter.addEventListener("click", filterAll);
-
-function filterShiniji() {
-    const Shiniji = Merch.filter(eva => eva.Shiniji === true);
-    filterFor(Shiniji);
-};
-DOMselectors.ShinijiFilter.addEventListener("click", filterShiniji);
-
-function filterREI() {
-    const REI = Merch.filter(eva => eva.REI === true);
-    filterFor(REI);
-};
-DOMselectors.REIFilter.addEventListener("click", filterREI);
-
-function filterAsuka() {
-    const Asuka = Merch.filter(eva => eva.Asuka === true);
-    filterFor(Asuka);
-};
-DOMselectors.AsukaFilter.addEventListener("click", filterAsuka);
+  async function main() {
+    var filtercharacters = await getData(url);
+    defaultCardGenerate(filtercharacters);
+  }
+  main();
